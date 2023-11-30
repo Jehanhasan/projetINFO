fe583363d955e18f1cdb73c24d1eb6729546b271
jehanhasan <cihanhesen1@gmail.com>
2023-11-30 12:02:14 +0100

    Recevoir

--- sketch_nov30a/sketch_nov30a.ino
+++ sketch_nov30a/sketch_nov30a.ino
@@ -1,75 +1,85 @@
 
 
-/* This file has been prepared for Doxygen automatic documentation generation.*/
-/*! \file *********************************************************************
-*
-* \brief
-*      laisison serie
-*
-*      This file contains the full implementation of the motor control,
-*      except the PID-controller.
-*
-* \par Application note:
-*      AVR447: Sinusoidal driving of three-phase permanent motor using
-*      ATmega48/88/168
-*
-* \par Documentation
-*      For comprehensive code documentation, supported compilers, compiler
-*      settings and supported devices see readme.html
-*
-* \author
-*      Atmel Corporation: cihanhesen1@gmail.com \n
-*      Support email: avr@atmel.com
-*
-* $Name: Jehan_1_0 $
-* $Revision: 1.8 $
-* $RCSfile: main.c,v $
-* $Date: 2006/03/27 07:20:51 $  \n
-******************************************************************************/
-
-
-
-#define FOSC 16000000 // Clock Speed
+/*! \brief fréquence du qwark du µcontroller*/
+#define FOSC 16000000
+/*! \brief nombre de changement d'état par seconde(vitesse d'envoi de données)*/
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-int main()
-{
-USART_Init(MYUBRR);
+/*! \brief calcul servant à trouver le numéro du registre*/
+#define MYUBRR FOSC / 16 / BAUD - 1
+unsigned char flag = 0;
+unsigned char data;
 
 
 
 
-while(1)
-{
-USART_Transmit(65);
+
+
+/*! \brief USART_INIT function / initialisation
+* la fonction permet d'aurotiser la transmission et de choisir le nombre de bit de stop avec à l'aide des registres UCSR0B et UCSR0C
+*/
+void USART_Init(unsigned int ubrr) {
+ /*Set baud rate */
+ UBRR0H = (unsigned char)(ubrr >> 8);
+ UBRR0L = (unsigned char)ubrr;
+ /*autoriser  transmitter allumer le bit n3 */
+ UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+ /* Set frame format: 8data, 2stop bit */
+ UCSR0C = (3 << UCSZ00);
+ DDRB = 0B00100000;
+ PORTB = 0B00000000;
+}
+
+
+/*! \brief USART_Transmit function / transmission
+la fonction sert à transmettre une chaine de caractère
+*/
+void USART_Transmit(unsigned char data) {
+ /* Wait for empty transmit buffer */
+ while (!(UCSR0A & (1 << UDRE0)))
+   ;
+ /* Put data into buffer, sends the data */
+ UDR0 = data;
+}
+
+
+/*! \brief USART_Receive function / recetption
+la fonction sert à recevoir une chaine de caractère
+*/
+unsigned char USART_Receive(void) {
+ /* Wait for data to be received */
+ while (!(UCSR0A & (1 << RXC0)))
+   ;
+ /* Get and return received data from buffer */
+ return UDR0;
 }
 
 
 
 
+/*! \brief fonction d'interruption
+la fonction sera éxécutée quand l'interruption sera cativée
+*/
+ISR(USART_RX_vect) {
+ data = UDR0;
+ flag = 1;
 }
 
 
+/*! \brief Main function
+utilisée pour appeler les fonctions init et et démarrer la transmmission
+*/
+int main(void) {
 
 
-void USART_Init(unsigned int ubrr)
-{
-/*Set baud rate */
-UBRR0H = (unsigned char)(ubrr>>8);
-UBRR0L = (unsigned char)ubrr;
-/*Enable receiver and transmitter */
-UCSR0B = (1<<TXEN0);
-/* Set frame format: 8data, 2stop bit */
-UCSR0C = (1<<USBS0)|(3<<UCSZ00);
-
-
-
-
+ USART_Init(MYUBRR);
+ sei();
+ while (1) {
+   if (flag == 1) {
+     /* PORTB=0B00100000;//Allumer la led quand l'interruption est réalisée*/
+     USART_Transmit(data + 1);
+     flag = 0;
+   }
+   _delay_ms(1);
+ }
 }
-void USART_Transmit(unsigned char data)
-{
-/* Wait for empty transmit buffer */
-while (!(UCSR0A & (1<<UDRE0)));
-/* Put data into buffer, sends the data */
-UDR0 = data;
-}
+
